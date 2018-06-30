0ebcaad3519ef84f43ae5e62e3624121d5ac9c1a
Archiebald Katleho KHANYE <akhanye@e5r5p1.wethinkcode.co.za>
2018-06-30 16:52:25 +0200

    Initial push

---
+++ src/main/java/com/swingy/app/Swingy.java
@@ -0,0 +1,11 @@
+/*
+ * Created by Katleho Khanye (akhanye)
+ * */
+
+package com.swingy.app;
+
+public class Swingy {
+	public static void main(String[] args) {
+		System.out.println("Hello you my friend!!!");
+	}
+}
