8aceffce9c52bbabc0a53be1c0df1d3aa30d1ced
Katleho <akhanye@student.wethinkcode.co.za>
2018-07-14 00:50:24 +0200

    changes to the swingy

--- src/main/java/com/swingy/app/views/PlayerViewGui.java
+++ src/main/java/com/swingy/app/views/PlayerViewGui.java
@@ -9,15 +9,19 @@ import javax.swing.*;
 import java.awt.event.*;
 import com.swingy.app.controllers.PlayerController;
 import com.swingy.app.models.PlayerModel;
+import com.swingy.app.models.ValidationErrorModel;
 import java.util.List;
+import java.util.ArrayList;
 
 public class PlayerViewGui extends PlayerView implements Display {
 
 	private PlayerController playerController;
+	private PlayerModel playerModel;
 	private JFrame 	frame;
 	private JPanel 	pnl = new JPanel();
 	private int 	returnNum;
-
+	private JTextField	nameTxt;
+	private JComboBox	classType;
 
 	public PlayerViewGui() {
 		super();
@@ -26,11 +30,57 @@ public class PlayerViewGui extends PlayerView implements Display {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	/*public Jpanel	namePanel() {
+		JLabel		nameLabel;
+		Jlabel		classLabel;
+		JTextBox	nameTxt;
+		JPanel		panel;
+
+		nameLabel = new JLabel("Name");
+		classLabel = new JLabel("Class");
+		nameTxt = new JTextBox(20);
+		nameLabel.setBounds(50, 50, 200, 30);
+		nameTxt.setBounds(200, 50, 200, 30);
+		classLabel.setBounds(50, 90, 200, 30);
+		panel.setSize(400, 400);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		panel.add(nameLabel);
+		panel.add(classLabel);
+		return(panel);
+	}*/
+
 	public void	createPlayer(PlayerModel playerModel) {
 		frame = new JFrame("New Player");
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel		nameLabel;
+		JLabel		classLabel;
+		JButton		saveBtn;
+		String		options[] = {"Knight", "Elf", "Viking"};
+		
+		this.playerModel = playerModel;
+		nameLabel = new JLabel("Name");
+		classLabel = new JLabel("Class");
+		nameTxt = new JTextField(20);
+		nameLabel = new JLabel("Name");
+		classLabel = new JLabel("Class");
+		nameTxt = new JTextField(20);
+		saveBtn = new JButton("SAVE");
+		classType = new JComboBox(options);
+		classType.setSelectedIndex(0);
+		nameLabel.setBounds(10, 50, 200, 30);
+		nameTxt.setBounds(200, 50, 200, 30);
+		classLabel.setBounds(10, 90, 200, 30);
+		saveBtn.setBounds(50, 150, 100, 30);
+		classType.setBounds(200, 90, 200, 30);
+		frame.add(nameLabel);
+		frame.add(nameTxt);
+		frame.add(classLabel);
+		frame.add(saveBtn);
+		frame.add(classType);
+		saveBtn.addActionListener(new btnAction());
 		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(500, 300));
+		frame.setLayout(null);
 		frame.pack();
 		frame.setVisible(true);
 	}
@@ -71,8 +121,30 @@ public class PlayerViewGui extends PlayerView implements Display {
 				case "Select Player":
 					break ;
 				case "Exit":
+					frame.setVisible(false);
+					frame.dispose();
 					break ;
 			}
+		}
+	}
+
+	private void savePlayerFields() {
+		List<ValidationErrorModel> errors;
+
+		System.out.println("Btn Pressed");
+		playerModel.setName(nameTxt.getText());
+		playerModel.setPClass(classType.getSelectedItem().toString());
+		errors = new ArrayList<ValidationErrorModel>();
+		if (!this.playerController.validatePlayer(errors)) {
+			System.out.println("Errors Found");
+			return ;
+		}
+		this.playerController.savePlayer();
+	}
+
+	public class btnAction implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			savePlayerFields();
 		}
 	}
 
