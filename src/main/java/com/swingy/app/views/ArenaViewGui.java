09266a830eb3efbcaabc935aff226d823aedfa06
Katleho <akhanye@student.wethinkcode.co.za>
2018-07-19 19:46:18 +0200

    Before commit

--- src/main/java/com/swingy/app/views/ArenaViewGui.java
+++ src/main/java/com/swingy/app/views/ArenaViewGui.java
@@ -2,8 +2,27 @@
  * Author Katleho Khanye (akhanye)
  * */
 
-package com.siwngy.app.view;
+package com.siwngy.app.views;
+
+import com.swingy.app.controllers.ArenaController;
+import com.swingy.app.models.PlayerModel;
 
 public class ArenaViewGui extends ArenaView implements Display {
+	public void	showMap(char[][] map, ArenaController arenaController, PlayerModel playerModel) {
+	}
 
+	public void	updateMap(char[][] map) {
+	}
+
+	public void prepareFight(String prepareString) {
+	}
+
+	public void	updateFight(String updateString) {
+	}
+
+	public void	showMessage(String message) {
+	}
+
+	public void	makeChoice(PlayerModel enemy, ArenaController arenaController) {
+	}
 }
